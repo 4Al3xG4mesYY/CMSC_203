9d38a5ba6efac784bea5caa397784939f8ba96e7
Alex <127744440+4Al3xG4mesYY@users.noreply.github.com>
2025-02-24 22:01:35 -0500

    Update MovieDriver_Task1.java

--- CMSC203_Lab1/MovieDriver_Task1.java
+++ CMSC203_Lab1/MovieDriver_Task1.java
@@ -1,3 +1,12 @@
+/*
+Class: CMSC203 CRN 202530
+Program: Lab 1
+Instructor: Grigoriy Grinberg 
+Summary of Description: (Making a driver for the Movie.java program) 
+Due Date: 02/24/2025 
+Integrity Pledge: I pledge that I have completed the programming assignment independently.
+I have not copied the code from a student or any source.
+*/
 package Movie;
 import java.util.Scanner;
 public class MovieDriver_Task1 
